182a3b0b9f31a3e05c1288d087bb35a9677646ed
Mehmet Ali KIZILARSLAN <mehmetali729@gmail.com>
2021-09-19 10:56:09 +0300

    Feed The Animals

---
+++ Prototype 2 CWC/Assets/Scripts/DestroyOutOfBoundary.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundary : MonoBehaviour
+{
+    private float topBoundary = 30.0f;
+    private float lowerBoundary = -10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > topBoundary)
+        {
+            Destroy(gameObject);
+        } else if(transform.position.z < lowerBoundary)
+        {
+            Destroy(gameObject);
+            Time.timeScale = 0;
+        }
+    }
+}
